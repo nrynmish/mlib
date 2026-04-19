5dd5871f71aaa55059b9625498c47adcf560739d
nrynmish <narayan16sept@gmail.com>
2026-04-19 18:51:38 +0530

    implemented a value class with autogradient graph and backward propogation support

--- main.cpp
+++ main.cpp
@@ -5,12 +5,17 @@
 #include <cmath>
 #include <unordered_map>
 #include <functional>
+#include <unordered_set>
 
 class Value;
 
 using namespace std;
 
 using Valueptr = shared_ptr<Value>;
+
+struct Hash {
+    size_t operator()(const Valueptr value) const;
+};
 
 class Value : public enable_shared_from_this<Value> 
 {
@@ -20,7 +25,9 @@ class Value : public enable_shared_from_this<Value>
         float gradient;
         string operation;
         size_t id;
-        vector<Value> prev;
+        vector<Valueptr> prev;
+        function<void()> backward; 
+        friend struct Hash;  
     
     private:
         Value(float data, const string& operation, size_t id) : data(data), operation(operation), id(id){};
@@ -36,12 +43,87 @@ class Value : public enable_shared_from_this<Value>
             --currentID;
         }
 
+        static Valueptr add(const Valueptr& lhs, const Valueptr& rhs)
+        {
+            auto result = Value::create(lhs->data + rhs->data, "+");
+            result->prev = {lhs, rhs};
+            result->backward =[lhs_weak = weak_ptr<Value>(lhs), rhs_weak = weak_ptr<Value>(rhs), result_weak = weak_ptr<Value>(result)]()
+            {
+                lhs_weak.lock()->gradient += result_weak.lock()->gradient;
+                rhs_weak.lock()->gradient += result_weak.lock()->gradient;
+            };
+            return result;
+        }
+
+        static Valueptr mul(const Valueptr& lhs, const Valueptr& rhs)
+        {
+            auto result = Value::create(lhs->data * rhs->data, "*");
+            result->prev = {lhs, rhs};
+            result->backward = [lhs_weak = weak_ptr<Value>(lhs), rhs_weak = weak_ptr<Value>(rhs), result_weak = weak_ptr<Value>(result)]()
+            {
+                lhs_weak.lock()->gradient += rhs_weak.lock()->data * result_weak.lock()->gradient;
+                rhs_weak.lock()->gradient += lhs_weak.lock()->data * result_weak.lock()->gradient;
+            };
+            return result;
+        }
+
+        static Valueptr substract(const Valueptr& lhs, const Valueptr& rhs)
+        {
+            auto result = Value::create(lhs->data - rhs->data, "-");
+            result->prev = {lhs, rhs};
+            result->backward = [lhs_weak = weak_ptr<Value>(lhs), rhs_weak = weak_ptr<Value>(rhs), result_weak = weak_ptr<Value>(result)]()
+            {
+                lhs_weak.lock()->gradient += result_weak.lock()->gradient;
+                rhs_weak.lock()->gradient -= result_weak.lock()->gradient;
+            };
+            return result;
+        }
+
+        void buildTopo(Valueptr v, unordered_set<Valueptr, Hash>& visited, vector<Valueptr>& topo)
+        {
+            if(visited.find(v) == visited.end())
+            {
+                visited.insert(v);
+                for(const auto& child : v->prev){
+                    buildTopo(child, visited, topo);
+                }
+                topo.push_back(v);
+            }
+        }
+
+        void backProp()
+        {
+            vector<Valueptr> topo;
+            unordered_set<Valueptr, Hash> visited;
+            buildTopo(shared_from_this(),visited, topo);
+            for(auto nodes : topo)
+            {
+                nodes->backward();
+            }
+        }
+
+        void print() const
+        {
+            cout << "Value(data=" << data << ", grad=" << gradient << ", op=" << operation << ", id=" << id << ")" << endl;
+        }
+
+
     };
+
+size_t Hash::operator()(const Valueptr value) const
+{
+    return hash<string>()(value.get()->operation) ^ hash<float>()(value.get()->data);
+}
 
 int main(){
     
     auto a = Value::create(1.0, "+");
-    // auto c = add(a + b);
+    auto b = Value::create(2.0, "+");
 
+    auto c = Value::add(a, b);
+    c->print();
+
+    auto d = Value::mul(c, c);
+    d->print();
     // auto a = shared_ptr<Value>();
 }
